2ed2d7bbf7e53e729adc63c665112b1d5035e65c
FelipeCalvache <felipecalvache6@gmail.com>
2025-02-01 06:26:09 -0500

    Add project structure

---
+++ src/App.tsx
@@ -0,0 +1,221 @@
+// import { useState } from 'react'
+import "./App.css";
+import {
+  Box,
+  FileUpload,
+  For,
+  SimpleGrid,
+  Table,
+  Tabs,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { LuFolder, LuSquareCheck, LuUser } from "react-icons/lu";
+import { ExcelUploadForm } from "./components/ui/ExcelUploadForm";
+
+interface Employee {
+  id: string;
+  name: string;
+  identification: string;
+  password: string;
+  role: string;
+}
+
+interface Project {
+  id: string;
+  name: string;
+  costCenter: string;
+}
+
+interface ProcessedWorkDay {
+  employeeName: string;
+  employeeIdentification: string;
+  projectName: string;
+  duration: number;
+}
+
+const App: React.FC = () => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [workDays, setWorkDays] = useState<ProcessedWorkDay[]>([]);
+  const [upDateData, setUpDateData] = useState<boolean>(false)
+
+  const [loading, setLoading] = useState<boolean>(true); // Estado de carga
+  const [error, setError] = useState<string | null>(null); // Estado de error
+
+  // 📌 useEffect se ejecuta cuando el componente se monta
+  useEffect(() => {
+    fetch("http://localhost:8080/api/employees")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setEmployees(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/projects")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProjects(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/employees/workdays")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWorkDays(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [upDateData]);
+
+  function handleSetUpDateData(){
+    console.log("Se actulaizo el estado de handleSetUpDateData");
+    setUpDateData(!upDateData)
+  }
+
+  return (
+    <Tabs.Root key={"line"} defaultValue="members" variant={"line"}>
+      <Tabs.List>
+        <Tabs.Trigger value="members">
+          <LuUser />
+          Empleados
+        </Tabs.Trigger>
+        <Tabs.Trigger value="projects">
+          <LuFolder />
+          Projects
+        </Tabs.Trigger>
+        <Tabs.Trigger value="tasks">
+          <LuSquareCheck />
+          Horas por empleado
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="members">
+        <Table.Root size="sm" striped>
+          <Table.Header>
+            <Table.Row>
+              <For
+                each={
+                  employees && employees.length > 0
+                    ? Object.keys(employees[0])
+                    : []
+                }
+              >
+                {(key) => (
+                  <Table.ColumnHeader key={key}>{key}</Table.ColumnHeader>
+                )}
+              </For>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {employees?.map((item) => (
+              <Table.Row key={item.id}>
+                <Table.Cell key={item.id}>{item.id}</Table.Cell>
+                <Table.Cell key={item.name}>{item.name}</Table.Cell>
+                <Table.Cell key={item.identification}>
+                  {item.identification}
+                </Table.Cell>
+                <Table.Cell key={item.password}>{item.password}</Table.Cell>
+                <Table.Cell textAlign="end">{item.role}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Tabs.Content>
+      <Tabs.Content value="projects">
+        <Table.Root size="sm" striped>
+          <Table.Header>
+            <Table.Row>
+              <For
+                each={
+                  projects && projects.length > 0
+                    ? Object.keys(projects[0])
+                    : []
+                }
+              >
+                {(key) => <Table.ColumnHeader>{key}</Table.ColumnHeader>}
+              </For>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {projects?.map((item) => (
+              <Table.Row key={item.id}>
+                <Table.Cell key={item.id}>{item.id}</Table.Cell>
+                <Table.Cell key={item.name}>{item.name}</Table.Cell>
+                <Table.Cell key={item.costCenter}>{item.costCenter}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Tabs.Content>
+      <Tabs.Content value="tasks">
+        <ExcelUploadForm
+          handleSetUpDateData={handleSetUpDateData}
+        ></ExcelUploadForm>
+        <Table.Root size="sm" striped>
+          <Table.Header>
+            <Table.Row>
+              <For
+                each={
+                  workDays && workDays.length > 0
+                    ? Object.keys(workDays[0])
+                    : []
+                }
+              >
+                {(key) => <Table.ColumnHeader>{key}</Table.ColumnHeader>}
+              </For>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {workDays?.map((item) => (
+              <Table.Row
+                // backgroundColor={item.end ? "none" : "red"}
+                key={item.id}
+              >
+                <Table.Cell>{item.id}</Table.Cell>
+                <Table.Cell>{item.start.date}</Table.Cell>
+                {item.end ? (
+                  <Table.Cell>{item.end?.date}</Table.Cell>
+                ) : (
+                  <Table.Cell>⚠️Revisar</Table.Cell>
+                )}
+                <Table.Cell>{item.employee.name}</Table.Cell>
+                <Table.Cell>{item.duration}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+};
+
+export default App;
---
+++ src/components/ui/ExcelUploadForm.tsx
@@ -0,0 +1,71 @@
+import { useState, ChangeEvent, FormEvent, FC } from "react";
+import { Box, Button, Input } from "@chakra-ui/react";
+
+interface ChildProps {
+  handleSetUpDateData: () => void;
+}
+
+export const ExcelUploadForm : FC<ChildProps> = ({ handleSetUpDateData }) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      console.log("entro al chage");
+      setFile(selectedFile);
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!file) {
+      console.log("No se cargo archivo");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await fetch(
+        "http://localhost:8080/api/attendance/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Error al subir el archivo: ${response.statusText}`);
+      }
+      console.log("Subida exitosamente");
+      handleSetUpDateData();
+    } catch (error) {
+      console.error("Error en la subida", error);
+    } finally {
+      setLoading(false);
+      setFile(null);
+    }
+  };
+
+  return (
+    <Box maxW="md" mx="auto" p={4}>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="file"
+          accept=".xlsx,.xls"
+          onChange={handleFileChange}
+          disabled={loading}
+          padding={1}
+          
+        />
+
+        <Button mt={4} type="submit" color="red">
+          Subir
+        </Button>
+      </form>
+    </Box>
+  );
+};
